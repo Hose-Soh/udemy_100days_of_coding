45032ab03e6865ac61a946d92cf2d1c13e98d5ca
Hose-Soh <sohrawardy.mhose@gmail.com>
2024-09-02 15:39:30 +0400

    adding day 6 codes

---
+++ day_6/reeborgs_world.py
@@ -0,0 +1,109 @@
+# #hurdle 1
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def hurdle():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# while(not at_goal()):
+#     hurdle()
+
+# # hurdle2
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def hurdle():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# while(not at_goal()):
+#     hurdle()
+
+     
+# # hurdle 3
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def hurdle():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# while(not at_goal()):
+#     if wall_in_front():
+#         hurdle()
+#     else: 
+#         move()
+
+     
+# # hurdle 4
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def hurdle():
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while (not wall_in_front()):
+#         move()
+#     if wall_in_front():
+#         turn_left()
+    
+    
+
+# while(not at_goal()):
+#     if wall_in_front():
+#         hurdle()
+#     else: 
+#         move()
+
+     
+    
+
+# # maze
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+
+# while not(at_goal()):
+#     if front_is_clear():
+#         move()
+#         if right_is_clear():
+#             turn_right()
+#     elif right_is_clear():
+#         turn_right()
+#     else:
+#         turn_left()
+
+     
+    
---
+++ day_6/task_1.py
@@ -0,0 +1,4 @@
+def first_function():
+    print("First Function\n")
+
+first_function()
