298e92b4c9e169f4c40ea9a52692ad26a504bce4
Hose-Soh <sohrawardy.mhose@gmail.com>
2024-09-04 18:33:17 +0400

    add day8 codes

---
+++ day_8/task_1.py
@@ -0,0 +1,15 @@
+# define a function to print
+def greet():
+    print("Hello Jorge")
+    print("How are you?")
+    print("It's been so long we've met!")
+
+greet()
+
+# define another function with a parameter
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How are you?")
+    print(f"It's been so long we've met!")
+
+greet_with_name("Nikhil")
